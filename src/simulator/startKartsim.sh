f8fda1d4ee3e8fed1b13921e3dec64a7eb03f57c
Michael von Bueren <vbueremi@student.ethz.ch>
2019-04-26 19:30:56 +0200

    changed choice for whether to show visualizaiton and or log

--- src/simulator/startKartsim.sh
+++ src/simulator/startKartsim.sh
@@ -12,22 +12,31 @@ PREPROFOLDERPATH=$PREPROROOT/$PREPROFOLDER
 SAVEPATH="/home/mvb/0_ETH/01_MasterThesis/SimData"
 FOLDERNAME=$(date +"%Y%m%d-%H%M%S")
 FOLDERPATH=$SAVEPATH/$FOLDERNAME
-SIMTAG="_validation"
+SIMTAG="_test"
 SAVEFOLDERPATH=$SAVEPATH/$FOLDERNAME$SIMTAG
 SIMLOGFILENAMES=()
-for i in $PREPROFOLDERPATH/*.pkl; do SIMLOGFILENAMES+="$(basename "$i"),"; done;
+
+#select files
+ITER=0
+FIRSTFILE=2
+LASTFILE=2
+for i in $PREPROFOLDERPATH/*.pkl; do if ((ITER >= $FIRSTFILE && ITER <= $LASTFILE)); then SIMLOGFILENAMES+=($(basename "$i")); fi; ITER=$(( $ITER + 1)); done;
 #SIMLOGFILENAMES=$(cd $PREPROFOLDERPATH; ls -l | egrep -v '^d')
 #echo $SIMLOGFILENAMES
-VISUALIZATION=true
-#VISUALIZATION=false
+VISUALIZATION=1
+LOGGING=1
 
 mkdir $SAVEFOLDERPATH
-cp $PREPROFOLDERPATH/* $SAVEFOLDERPATH
+#for i in $SIMLOGFILENAMES; do cp $PREPROFOLDERPATH/$i $SAVEFOLDERPATH; done
+#IFS=', ' read -r -a array <<< "$SIMLOGFILENAMES"
+#echo "${array[@]}"
+#for i in "${array[@]}"; do echo "${i}"; echo "p"; done
+for i in "${SIMLOGFILENAMES[@]}"; do cp $PREPROFOLDERPATH/"${i}" $SAVEFOLDERPATH; done
 
-python basicKartsimServer.py $VISUALIZATION &
+python basicKartsimServer.py $VISUALIZATION $LOGGING &
 SRVPID=$!
 
-if [ $VISUALIZATION = true ]
+if (( $VISUALIZATION ))
 then
     python basicVisualizationClient.py &
     VIZPID=$!
@@ -37,7 +46,8 @@ python basicLoggerClient.py $SAVEFOLDERPATH "${SIMLOGFILENAMES[@]}" &
 LOGPID=$!
 
 #python basicSimulationClient.py $SAVEFOLDERPATH $PREPROFOLDERPATH &
-python validationClient.py $SAVEFOLDERPATH $PREPROFOLDERPATH &
+python evaluationClient.py $SAVEFOLDERPATH $PREPROFOLDERPATH "${SIMLOGFILENAMES[@]}" &
+#echo $SAVEFOLDERPATH $PREPROFOLDERPATH "${SIMLOGFILENAMES[@]}"
 SIMPID=$!
 
 exit_script() {
