3e9638465f2a5cdd9bb64d4e54f9ddc134e93272
MvBue <vbueremi@student.ethz.ch>
2019-05-04 22:15:02 +0200

    minor changes

--- src/dataanalysis/pyqtgraph/evaluation.py
+++ src/dataanalysis/pyqtgraph/evaluation.py
@@ -12,7 +12,6 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
 def main():
-
     pathrootsimdata = '/home/mvb/0_ETH/01_MasterThesis/SimData'
     simfolders = dIO.getDirectories(pathrootsimdata)
     simfolders.sort()
@@ -30,26 +29,32 @@ def main():
                 pklfiles.append([os.path.join(r, file), file])
     csvfiles.sort()
     pklfiles.sort()
+    simfiles = []
+    for i in range(len(pklfiles)):
+        simname = pklfiles[i][1][:18]
+        simfiles.append([pklfiles[i]])
+        for j in range(len(csvfiles)):
+            if csvfiles[j][1][:18] == simname:
+                simfiles[i].append(csvfiles[j])
+    print(simfiles[0][0][0])
 
-
-    for index in range(len(pklfiles)):
+    for index in range(len(simfiles)):
         try:
-            rawdataframe = dIO.getPKL(pklfiles[index][0])
+            rawdataframe = dIO.getPKL(simfiles[index][0][0])
         except:
-            print('EmptyDataError: could not read data from file ', pklfiles[index][0])
+            print('FileNotFoundError: could not read data from file ', simfiles[index][0][0])
             raise
 
 
-        resultsfilepath = pathsimdata +'/evaluationresults.txt'
         rawvaldata = []
         simvaldata = []
         diff = []
         rmse = []
-        for j in range(2):
+        for j in range(len(simfiles[index][1:])):
             try:
-                simdataframe = dIO.getCSV(csvfiles[index*2+j][0])
+                simdataframe = dIO.getCSV(simfiles[index][j+1][0])
             except:
-                print('EmptyDataError: could not read data from file ', csvfiles[index*2+j][0])
+                print('FileNotFoundError: could not read data from file ', simfiles[index][j+1][0])
                 raise
 
             start = 0
@@ -60,22 +65,21 @@ def main():
             # print(np.square(diff))
             rmse.append(np.sqrt(np.sum(np.square(diff[j])) / len(rawdataframe[start:start + part])))
 
-            print('Evaluation for ', csvfiles[index * 2 + j][1], 'vs', pklfiles[index][1])
+            print('Evaluation for ', simfiles[index][j+1][1], 'vs', simfiles[index][0][1])
             print(rmse[j])
             print('Overall score: ', np.sum(rmse[j]), '\n')
 
-
         #generating results page
-        pdffilepath = pathsimdata + '/' + pklfiles[index][1][:-19] + '_evaluation_plots.pdf'
+        pdffilepath = pathsimdata + '/' + simfiles[index][0][1][:-19] + '_evaluation_plots.pdf'
         pdf = PdfPages(pdffilepath)
         infotext1 = simfolder + '\n'
-        for k in range(2):
+        for k in range(len(simfiles[index][1:])):
             if k == 0:
                 infotext2 = '\n\n\nRMSE\n'
             else:
                 infotext2 += '\n\nRMSE\n'
             infotext1 += '_____________________________________\n'
-            infotext1 +='Evaluation for ' + csvfiles[index * 2 + k][1] + ' vs ' + pklfiles[index][1] + '\n'
+            infotext1 +='Evaluation for ' + simfiles[index][k+1][1] + ' vs ' + simfiles[index][0][1] + '\n'
             infotext1 += 'Parameter' + '\n'
             for i in range(len(rmse[k])):
                 infotext1 += str(rmse[k].index[i]) + '\n'
@@ -87,13 +91,14 @@ def main():
         resultsPage.text(0.05, 0.95, infotext1,  size=12, ha="left", va='top')
         resultsPage.text(0.2, 0.95, infotext2,  size=12, ha="left", va='top')
         pdf.savefig()
-        # print('Evaluation for ' + csvfiles[index * 2 + k][1] + ' vs ' + pklfiles[index][1] + '\n')
+        plt.close()
+        # print('Evaluation for ' + simfiles[index][k+1][1] + ' vs ' + simfiles[index][0][1] + '\n')
         # print(rmse)
 
 
         # generating plots
         for debugtopic in rawvaldata[0].columns:
-            for k in range(2):
+            for k in range(len(simfiles[index][1:])):
                 # debugtopic = 'pose vtheta'
                 savetopic = debugtopic.replace(' ', '_')
                 fig, axs = plt.subplots(2, 1, figsize = (10,6))
@@ -105,11 +110,13 @@ def main():
                 axs[1].plot(diff[k][debugtopic], color = 'r', label = 'error')
                 axs[1].legend()
                 pdf.savefig()
+                plt.close()
                 # plt.savefig(pathsimdata + '/' + savetopic + '-' + csvfiles[index * 2 + j][1][:-4] + '.pdf')
                 # plt.show()
 
 
         #results page as txt file
+        # resultsfilepath = pathsimdata +'/evaluationresults.txt'
         # for k in range(2):
         #     try:
         #         fh = open(resultsfilepath, 'r')
@@ -118,14 +125,14 @@ def main():
         #             os.remove(resultsfilepath)
         #         with open(pathsimdata + '/evaluationresults.txt', 'a') as the_file:
         #             the_file.write('_____________________________________\n')
-        #             the_file.write('Evaluation for ' + csvfiles[index*2+j][1] + ' vs ' + pklfiles[index][1] + '\n')
+        #             the_file.write('Evaluation for ' + csvfiles[index*2+j][1] + ' vs ' + simfiles[index][0][1] + '\n')
         #             the_file.write('Parameter      RMSE' + '\n')
         #             the_file.write(str(rmse[k].to_string()) + '\n')
         #             the_file.write('Overall score: ' + str(rmse[k].sum()) + '\n\n')
         #     except FileNotFoundError:
         #         with open(pathsimdata + '/evaluationresults.txt', 'a') as the_file:
         #             the_file.write('_____________________________________\n')
-        #             the_file.write('Evaluation for ' + csvfiles[index*2+j][1] + ' vs ' + pklfiles[index][1] + '\n')
+        #             the_file.write('Evaluation for ' + csvfiles[index*2+j][1] + ' vs ' + simfiles[index][0][1] + '\n')
         #             the_file.write('Signal         RMSE' + '\n')
         #             the_file.write(str(rmse[k].to_string()) + '\n')
         #             the_file.write('Overall score: ' + str(rmse[k].sum()) + '\n\n')
