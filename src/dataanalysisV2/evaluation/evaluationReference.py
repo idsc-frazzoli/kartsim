8cbe942199c8f047150e44f8924f4347f5e60bdb
MvBue <vbueremi@student.ethz.ch>
2019-05-09 23:23:28 +0200

    references changed

--- src/dataanalysisV2/evaluation/evaluationReference.py
+++ src/dataanalysisV2/evaluation/evaluationReference.py
@@ -7,7 +7,7 @@ Created on Thu Mar 28 13:20:04 2019
 """
 import numpy as np
 import os
-from dataIO import getPKL, getDirectories
+from dataanalysisV2.dataIO import getPKL, getDirectories
 
 
 def main():
