7f81f029032cb01a4809ef4689cb187751951a92
MvBue <vbueremi@student.ethz.ch>
2019-05-10 19:02:46 +0200

    added sortOutData

--- src/dataanalysisV2/gokartpreprocessing/run.py
+++ src/dataanalysisV2/gokartpreprocessing/run.py
@@ -7,6 +7,7 @@ Created 09.05.19 10:18
 """
 from dataanalysisV2.dataIO import dict_from_csv, dict_to_csv, create_folder_with_time, dict_to_pkl
 from dataanalysisV2.gokartpreprocessing.sortout import sort_out
+from dataanalysisV2.gokartpreprocessing.sampleFromDataset import sample_from_logdata
 from dataanalysisV2.gokartpreprocessing.buildDataSet import stirData
 
 import time
@@ -18,18 +19,28 @@ def main():
 
     pathRootData = '/home/mvb/0_ETH/01_MasterThesis/Logs_GoKart/LogData/dynamics_newFormat/cuts'  # path where all the raw logfiles are
 
-    # preprocess data and compute inferred data from raw logs
-    preprocessData = False
-    requiredList = ['pose x', 'pose y', 'pose theta', 'vehicle vx', 'vehicle vy', 'pose vtheta', 'vmu ax',
-                    'vmu ay', 'pose atheta', 'MH AB', 'MH TV', 'MH BETA', ]  # list of required raw log signals
-    saveDatasetPath = '/home/mvb/0_ETH/01_MasterThesis/Logs_GoKart/LogData/PreprocessedData'
-    datasetTag = 'test_newFormat'
-
     # check logs for missing or incomplete data
-    sortOutData = True  # if True: checks all the raw logfiles for missing/incomplete data if not checked yet
-    sortOutDataOverwrite = True  # if True: all data in preproParams-file will be overwritten
-    preproParamsFileName = 'preproParams'
-    preproParamsFilePath = pathRootData + '/' + preproParamsFileName + '.csv'  # file where all the information about missing/incomplete data is stored
+    # sortOutData = True  # if True: checks all the raw logfiles for missing/incomplete data if not checked yet
+    sortOutData = False
+    # sortOutDataOverwrite = True  # if True: all data in preproParams-file will be overwritten
+    sortOutDataOverwrite = False
+    _preproParamsFileName = 'preproParams'
+    preproParamsFilePath = pathRootData + '/' + _preproParamsFileName + '.csv'  # file where all the information about missing/incomplete data is stored
+
+    # preprocess data and compute inferred data from raw logs
+    preprocessData = True
+    # preprocessData = False
+    required_list = ['pose x', 'pose y', 'pose theta', 'vehicle vx', 'vehicle vy', 'vehicle slip angle', 'pose vtheta',
+                     'vehicle ax local', 'vehicle ay local', 'pose atheta', 'MH AB', 'MH TV', 'MH BETA']  # list of signals and tags which should be true for the logs used to build the dataset
+    nono_list = ['reverse']
+    saveDatasetPath = '/home/mvb/0_ETH/01_MasterThesis/Logs_GoKart/LogData/PreprocessedData' # parent directory where the preprocessed data should be saved to (separate folder will be created in this directory)
+    dataset_tag = 'test_newFormat'
+
+    # sample data
+    sampleData = True
+    sampling_time_period = 0.01
+    path_sampled_data = '/home/mvb/0_ETH/01_MasterThesis/Logs_GoKart/LogData/DataSets' # parent directory where the sampled data should be saved to (separate folder will be created in this directory)
+
 
     # ______________^^^_________________
 
@@ -48,16 +59,20 @@ def main():
         print('preproParams saved to ', preproParamsFilePath)
 
     if preprocessData:
-        print('Data preprocessing started...', end='\r')
-        kartDataAll = stirData(pathRootData, preproParams, requiredList)
+        print('Data preprocessing started...')
+        kartDataAll = stirData(pathRootData, preproParams, required_list, nono_list)
 
         print('Data preprocessing completed.')
-        folder_path = create_folder_with_time(saveDatasetPath, datasetTag)
+        path_preprocessed_dataset = create_folder_with_time(saveDatasetPath, dataset_tag)
 
-        print('Now writing to file at', folder_path)
-        dict_to_pkl(kartDataAll, folder_path)
+        print('Now writing to file at', path_preprocessed_dataset)
+        dict_to_pkl(kartDataAll, path_preprocessed_dataset)
 
-    print('Total computing time: ', time.time() - t)
+    if sampleData:
+        print('Sampling Data...')
+        sample_from_logdata(sampling_time_period, path_sampled_data, path_preprocessed_dataset, dataset_tag)
+
+    print('Total computing time: ', int(time.time() - t), "s")
 
 if __name__ == "__main__":
     main()
