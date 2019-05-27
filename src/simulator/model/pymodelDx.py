d0ad817e7ec6d74e7323940cc254db6acc346a4b
MvBue <vbueremi@student.ethz.ch>
2019-05-27 17:08:11 +0200

    option for stable behaviour around zero velocity as well as backwards driving

--- src/simulator/model/pymodelDx.py
+++ src/simulator/model/pymodelDx.py
@@ -15,10 +15,10 @@ def marc_vehiclemodel (V, U):
     # res = [7, 10, 1] #second best human guess so far
     # res = [5.47243906141646,	8.63088711471141,	0.912601312957689] #best optimization so far
     # res = [8.0*1.1, 10.0*1.2, 1.0] #best human guess so far
-    res = [7,10.8888888888889,0.8] #test
-    D1 = res[0]
-    D2 = res[1]
-    Ic = res[2]
+    # res = [7,10.8888888888889,0.8] #test
+    # D1 = res[0]
+    # D2 = res[1]
+    # Ic = res[2]
 
 
     #Optimal parameters (from Marc)
@@ -36,9 +36,9 @@ def marc_vehiclemodel (V, U):
     C1 = 1
     B2 = 5.2
     C2 = 1.1
-    # D2 = 10;
-    # D1 = 10;
-    # Ic = 0.3
+    D2 = 10
+    D1 = 10
+    Ic = 0.3
 
 
     param = [B1,C1,D1,B2,C2,D2,Ic]
@@ -62,19 +62,23 @@ def pymodelDx(V, U, param):
     D2 = param[5]
     Ic = param[6]
 
-    reg = 0.2 #default: 0.5
+    # reg = 0.2 #default: 0.5
+    reg = 0.5
 
     l = 1.19
     l1 = 0.73
     l2 = l-l1
     f1n = l2/l
     f2n = l1/l
-    w = 1.08
-    # w = 0.
+    # w = 1.08
+    w = 1.0
     vel1 = np.matmul(rotmat(BETA),np.array([[VELX],[VELY+l1*VELROTZ]]))
     f1y = simplefaccy(vel1[1],vel1[0])
 
-    F1 = np.matmul(rotmat(-BETA),np.array([[0],[f1y[0]]]))*f1n
+    if isinstance(f1y,np.ndarray):
+        f1y = f1y[0]
+
+    F1 = np.matmul(rotmat(-BETA),np.array([[0],[f1y]]))*f1n
     F1x = F1[0]
     F1y = F1[1]
 
@@ -85,18 +89,13 @@ def pymodelDx(V, U, param):
     F2y2 = simpleaccy(VELY-l2*VELROTZ,VELX,(AB-TV/2.)/f2n)*f2n/2.
     #F2y2 = simpleaccy(VELY - l2 * VELROTZ, VELX, (AB) / f2n) * f2n / 2.
     F2y = simpleaccy(VELY-l2*VELROTZ,VELX,AB/f2n)*f2n
-    # print('F2y',F2y*l2)
+    # if abs(VELX) < 0.05 and abs(VELY) < 0.05:
+    #     TV = 0.
     TVTrq = TV*w
-    # print('tv',TVTrq)
 
     ACCROTZ = (TVTrq + F1y * l1 - F2y * l2) / Ic
     ACCX = F1x+F2x+VELROTZ*VELY
     ACCY = F1y+F2y1+F2y2-VELROTZ*VELX
-
-    if VELX < 0.08:
-        ACCX[0] = 0.0
-        ACCY[0] = 0.0
-        ACCROTZ[0] = 0.0
 
     return ACCX[0],ACCY[0],ACCROTZ[0]
 
@@ -108,11 +107,14 @@ def magic(s, B, C, D):
 
 
 def capfactor(taccx):
-    return (1 - satfun((taccx / D2) ** 2)) ** (1 / 2)
+    return (1.0 - satfun((taccx / D2) ** 2.0)) ** (1.0 / 2.0)
 
 
 def simpleslip(VELY, VELX, taccx):
-    return -(1 / capfactor(taccx)) * VELY / (VELX + reg)
+    # if abs(VELX) < 0.05 and abs(VELY) < 0.05:
+    #     VELY = 0.
+    #     VELX = 1.
+    return -(1 / capfactor(taccx)) * VELY / (abs(VELX) + reg)
 
 
 def simplediraccy(VELY, VELX, taccx):
@@ -124,7 +126,10 @@ def simpleaccy(VELY, VELX, taccx):
 
 
 def simplefaccy(VELY, VELX):
-    return magic(-VELY / (VELX + reg), B1, C1, D1)
+    # if abs(VELX) < 0.05 and abs(VELY) < 0.05:
+    #     VELY = 0.
+    #     VELX = reg + 1.
+    return magic(-VELY / (abs(VELX) + reg), B1, C1, D1)
 
 
 def satfun(x):
@@ -142,3 +147,19 @@ def satfun(x):
     else:
         print('ERROR: x in satfun(x) is not float!')
     return y
+
+def satfun_approx(x):
+    a = 1.03616764e+00
+    b = -1.03619930e+00
+    c = -2.91844486e-01
+    d = 7.27342214e+00
+    e = -1.04920374e+00
+    f = 3.59055456e-01
+    g = 4.58406776e-01
+    h = -3.10005810e-02
+    i = 1.70845190e-02
+    j = -9.54399532e+01
+    k = 6.32433105e-01
+    l = -1.97634176e+01
+    m = -4.60294363e-02
+    return (a * x + b) * (c * np.arctan(d * (x + e) + f) + g) + (h * np.arctan(i * (x + j) + k) + l) * (m)
