52274878421d77a8cae19ba050eef13b021cb481
MvBue <vbueremi@student.ethz.ch>
2019-06-11 18:08:13 +0200

    MAJOR CHANGE: model now takes steering encoder, brake position and motor currents as inputs and can handle vectorized inputs

--- src/simulator/model/dynamic_mpc_model.py
+++ src/simulator/model/dynamic_mpc_model.py
@@ -6,6 +6,7 @@ Created 07.06.19 15:54
 @author: mvb
 """
 import numpy as np
+from simulator.model.dynamic_model_input_converter import MotorFunction, BrakeFunction, SteeringFunction
 
 class DynamicVehicleMPC:
     
@@ -24,58 +25,76 @@ class DynamicVehicleMPC:
         
         self.regularization_factor = 0.5
 
+        self.motor_function = MotorFunction().get_vectorized_motor_function()
+        self.brake_function = BrakeFunction().get_vectorized_brake_function()
+        self.steering_function = SteeringFunction().get_vecortized_steering_function()
+
     def get_name(self):
         return self.name
 
     def get_accelerations(self, initial_velocities=[0,0,0], system_inputs=[0,0,0,0]):
-        velx, vely, velrotz = initial_velocities
-        turning_angle, acceleration_rear_axle, torque_tv = system_inputs
+        if isinstance(initial_velocities, list):
+            velx, vely, velrotz = initial_velocities
+            steering_angle, brake_position, motor_current_l, motor_current_r = system_inputs
+        else:
+            velx = initial_velocities[:, 0]
+            vely = initial_velocities[:, 1]
+            velrotz = initial_velocities[:, 2]
+            steering_angle = system_inputs[:, 0]
+            brake_position = system_inputs[:, 1]
+            motor_current_l = system_inputs[:, 2]
+            motor_current_r = system_inputs[:, 3]
 
-        # steering_angle, brake_position, motor_current_l, motor_current_r = system_inputs
-        # turning_angle, acceleration_rear_axle, torque_tv = self.transform_inputs(steering_angle,
-        #                                                                          brake_position,
-        #                                                                          motor_current_l,
-        #                                                                          motor_current_r)
+        turning_angle, acceleration_rear_axle, torque_tv = self.transform_inputs(steering_angle,
+                                                                                 brake_position,
+                                                                                 motor_current_l,
+                                                                                 motor_current_r,
+                                                                                 velx)
 
-        vel1 = np.matmul(self._rotmat(turning_angle), np.array([[velx], [vely + self.dist_cog_front_axle * velrotz]]))
-        f1y = self._simplefaccy(vel1[1], vel1[0])
+        f1_velx = np.sum(np.multiply(self._rotmat(turning_angle)[0],
+                                     np.array([velx, vely + self.dist_cog_front_axle * velrotz])), axis=0)
+        f1_vely = np.sum(np.multiply(self._rotmat(turning_angle)[1],
+                                     np.array([velx, vely + self.dist_cog_front_axle * velrotz])), axis=0)
+        f1y = self._simplefaccy(f1_vely, f1_velx)
 
-        if isinstance(f1y, np.ndarray):
-            f1y = f1y[0]
-        F1 = np.matmul(self._rotmat(-turning_angle), np.array([[0], [f1y]])) * self.weight_portion_front
-        F1x = F1[0]
-        F1y = F1[1]
+        if isinstance(f1y, np.float64):
+            F1x = np.sum(np.multiply(self._rotmat(-turning_angle)[0], np.array([np.zeros(1), f1y])),
+                         axis=0) * self.weight_portion_front
+            F1y = np.sum(np.multiply(self._rotmat(-turning_angle)[1], np.array([np.zeros(1), f1y])),
+                         axis=0) * self.weight_portion_front
+        else:
+            F1x = np.sum(np.multiply(self._rotmat(-turning_angle)[0], np.array([np.zeros(len(f1y)), f1y])),
+                         axis=0) * self.weight_portion_front
+            F1y = np.sum(np.multiply(self._rotmat(-turning_angle)[1], np.array([np.zeros(len(f1y)), f1y])),
+                         axis=0) * self.weight_portion_front
 
         F2x = acceleration_rear_axle
 
-        F2y1 = self._simpleaccy(vely - self.dist_cog_rear_axle * velrotz,
-                                velx,
-                                (acceleration_rear_axle + torque_tv / 2.) / self.weight_portion_rear) \
-               * self.weight_portion_rear / 2.
-
-        F2y2 = self._simpleaccy(vely - self.dist_cog_rear_axle * velrotz,
-                                velx,
-                                (acceleration_rear_axle - torque_tv / 2.) / self.weight_portion_rear) \
-               * self.weight_portion_rear / 2.
-
-        F2y = self._simpleaccy(vely - self.dist_cog_rear_axle * velrotz,
-                               velx,
-                               acceleration_rear_axle / self.weight_portion_rear) \
-              * self.weight_portion_rear
-
-        # if abs(velx) < 0.05 and abs(vely) < 0.05:
-        #     torque_tv = 0.
+        F2y1 = self._simpleaccy(vely - self.dist_cog_rear_axle * velrotz, velx, (acceleration_rear_axle + torque_tv / 2.) / self.weight_portion_rear) * self.weight_portion_rear / 2.
+        F2y2 = self._simpleaccy(vely - self.dist_cog_rear_axle * velrotz, velx, (
+                    acceleration_rear_axle - torque_tv / 2.) / self.weight_portion_rear) * self.weight_portion_rear / 2.
+        F2y = self._simpleaccy(vely - self.dist_cog_rear_axle * velrotz, velx,
+                               acceleration_rear_axle / self.weight_portion_rear) * self.weight_portion_rear
         TVTrq = torque_tv * self.track_width
 
         ACCROTZ = (TVTrq + F1y * self.dist_cog_front_axle - F2y * self.dist_cog_rear_axle) / self.params_intetia
         ACCX = F1x + F2x + velrotz * vely
-        ACCY = F1y + F2y1 + F2y2 - velrotz * velx
+        ACCY = F1y + (F2y1 + F2y2) - velrotz * velx
 
-        return ACCX[0],ACCY[0],ACCROTZ[0]
+        return [ACCX, ACCY, ACCROTZ]
 
-    def transform_inputs(self, steering_angle, brake_position, motor_current_l, motor_current_r):
+    def transform_inputs(self, steering_angle, brake_position, motor_current_l, motor_current_r, velx):
+        brake_acceleration = self.brake_function(brake_position)
 
-        return 0
+        acceleration_left_wheel = self.motor_function(velx, motor_current_l) - np.sign(velx) * brake_acceleration
+        acceleration_right_wheel = self.motor_function(velx, motor_current_r) - np.sign(velx) * brake_acceleration
+
+        acceleration_rear_axle = (acceleration_left_wheel + acceleration_right_wheel) / 2.0
+        torque_tv = (acceleration_right_wheel - acceleration_left_wheel) / 2.0
+
+        turning_angle = -0.63 * np.power(steering_angle, 3) + 0.94 * steering_angle
+
+        return turning_angle, acceleration_rear_axle, torque_tv
 
     def _rotmat(self, beta):
         return np.array([[np.cos(beta),np.sin(beta)],[-np.sin(beta), np.cos(beta)]])
@@ -83,32 +102,23 @@ class DynamicVehicleMPC:
     def _magic(self, s, params):
         return params[2] * np.sin(params[1] * np.arctan(params[0] * s))
 
-
     def _capfactor(self, taccx):
-        return (1.0 - self._satfun((taccx / self.params_tire_rear[2]) ** 2.0)) ** (1.0 / 2.0)
-
+        if isinstance(taccx, np.float64):
+            return (1.0 - self._satfun((taccx / self.params_tire_rear[2]) ** 2.0)) ** (1.0 / 2.0)
+        else:
+            return (1.0 - self._satfun_approx((taccx / self.params_tire_rear[2]) ** 2.0)) ** (1.0 / 2.0)
 
     def _simpleslip(self, VELY, VELX, taccx):
-        # if abs(VELX) < 0.05 and abs(VELY) < 0.05:
-        #     VELY = 0.
-        #     VELX = 1.
         return -(1 / self._capfactor(taccx)) * VELY / (abs(VELX) + self.regularization_factor)
-
 
     def _simplediraccy(self, VELY, VELX, taccx):
         return self._magic(self._simpleslip(VELY, VELX, taccx), self.params_tire_rear)
 
-
     def _simpleaccy(self, VELY, VELX, taccx):
         return self._capfactor(taccx) * self._simplediraccy(VELY, VELX, taccx)
 
-
     def _simplefaccy(self, VELY, VELX):
-        # if abs(VELX) < 0.05 and abs(VELY) < 0.05:
-        #     VELY = 0.
-        #     VELX = self.regularization_factor + 1.
         return self._magic(-VELY / (abs(VELX) + self.regularization_factor), self.params_tire_front)
-
 
     def _satfun(self, x):
         l = 0.8
@@ -126,18 +136,18 @@ class DynamicVehicleMPC:
             print('ERROR: x in _satfun(x) is not float!')
         return y
 
-# def satfun_approx(x):
-#     a = 1.03616764e+00
-#     b = -1.03619930e+00
-#     c = -2.91844486e-01
-#     d = 7.27342214e+00
-#     e = -1.04920374e+00
-#     f = 3.59055456e-01
-#     g = 4.58406776e-01
-#     h = -3.10005810e-02
-#     i = 1.70845190e-02
-#     j = -9.54399532e+01
-#     k = 6.32433105e-01
-#     l = -1.97634176e+01
-#     m = -4.60294363e-02
-#     return (a * x + b) * (c * np.arctan(d * (x + e) + f) + g) + (h * np.arctan(i * (x + j) + k) + l) * (m)
+    def _satfun_approx(self, x):
+        a = 1.03616764e+00
+        b = -1.03619930e+00
+        c = -2.91844486e-01
+        d = 7.27342214e+00
+        e = -1.04920374e+00
+        f = 3.59055456e-01
+        g = 4.58406776e-01
+        h = -3.10005810e-02
+        i = 1.70845190e-02
+        j = -9.54399532e+01
+        k = 6.32433105e-01
+        l = -1.97634176e+01
+        m = -4.60294363e-02
+        return (a * x + b) * (c * np.arctan(d * (x + e) + f) + g) + (h * np.arctan(i * (x + j) + k) + l) * (m)
