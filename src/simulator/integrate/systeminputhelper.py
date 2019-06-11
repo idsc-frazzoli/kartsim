9fdc97fe3f07f2595e6117159bd06f64ad90e79d
MvBue <vbueremi@student.ethz.ch>
2019-06-11 18:06:09 +0200

    MAJOR CHANGE: model inputs changed to steering encoder, brake position and motor currents

--- src/simulator/integrate/systeminputhelper.py
+++ src/simulator/integrate/systeminputhelper.py
@@ -9,17 +9,36 @@ from scipy.interpolate import interp1d
 import numpy as np
 
 def setInput(U):
-    global interpBETA, interpAB, interpTV
-    interpBETA = interp1d(U[0], U[1], fill_value='extrapolate')
-    interpAB = interp1d(U[0], U[2], fill_value='extrapolate')
-    # interpTV = interp1d(U[0], U[3], fill_value='extrapolate')
-    # interpBETA = [U[0], U[1]]
-    # interpAB = [U[0], U[2]]
-    interpTV = [U[0], U[3]]
+    # global interpBETA, interpAB, interpTV
+    # interpBETA = interp1d(U[0], U[1], fill_value='extrapolate')
+    # interpAB = interp1d(U[0], U[2], fill_value='extrapolate')
+    # # interpTV = interp1d(U[0], U[3], fill_value='extrapolate')
+    # # interpBETA = [U[0], U[1]]
+    # # interpAB = [U[0], U[2]]
+    # interpTV = [U[0], U[3]]
+
+    global interpSTEER, interpBRAKE, interpMOTL, interpMOTR
+    interpSTEER = interp1d(U[0], U[1], fill_value='extrapolate')
+    interpBRAKE = interp1d(U[0], U[2], fill_value='extrapolate')
+    interpMOTL = interp1d(U[0], U[3], fill_value='extrapolate')
+    interpMOTR = interp1d(U[0], U[4], fill_value='extrapolate')
+
 
 def getInput(time1):
-    index = np.argmax(interpTV[0]>time1-0.1)
+    # index = np.argmax(interpTV[0]>time1-0.1)
     # return [interpBETA(time1), interpAB(time1), interpTV(time1)]
     # return [interpBETA[1][index], interpAB[1][index], interpTV[1][index]]
     # print([interpBETA(time1), interpAB(time1), interpTV[1][index]])
-    return [interpBETA(time1), interpAB(time1), interpTV[1][index]]
+    # return [interpBETA(time1), interpAB(time1), interpTV[1][index]]
+    if abs(interpSTEER(time1)) < 0.01:
+        return [0.01, interpBRAKE(time1), interpMOTL(time1), interpMOTR(time1)]
+    return [interpSTEER(time1), interpBRAKE(time1), interpMOTL(time1), interpMOTR(time1)]
+
+def setInputAccel(A):
+    global interpX, interpY, interpTHETA
+    interpX = interp1d(A[0], A[1], fill_value='extrapolate')
+    interpY = interp1d(A[0], A[2], fill_value='extrapolate')
+    interpTHETA = interp1d(A[0], A[3], fill_value='extrapolate')
+
+def getInputAccel(time1):
+    return [interpX(time1), interpY(time1), interpTHETA(time1)]
