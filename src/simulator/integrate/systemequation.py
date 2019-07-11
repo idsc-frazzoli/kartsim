237fd46873bb58db0cbecd7e87e5c3dfa00ffc3c
MvBue <vbueremi@student.ethz.ch>
2019-07-11 13:27:27 +0200

    added Long Short Term Memory network

--- src/simulator/integrate/systemequation.py
+++ src/simulator/integrate/systemequation.py
@@ -21,8 +21,10 @@ class SystemEquation:
     #     print('initialized')
 
     def get_system_equation(self):
-        if self.vehicle_model_name in ["dynamic_vehicle_mpc", "data_driven_vehicle_model"]:
+        if self.vehicle_model_name in ["mpc_dynamic", "hybrid_mlp"]:
             return self.solveivp_dynamic_dx_dt
+        elif self.vehicle_model_name in ["hybrid_lstm"]:
+            return self.solveivp_hybrid_lstm_dx_dt
         elif self.vehicle_model_name == "kinematic_vehicle_mpc":
             return self.solveivp_kinematic_dx_dt
         elif self.vehicle_model_name == "acceleration_reference_model":
@@ -49,21 +51,33 @@ class SystemEquation:
 
         V_dt = self.vehicle_model.get_accelerations(V, U)
 
-        if t > 0.999:
-            print('_______')
-        if t < 0.5:
-            # print('t:{:5.4f}'.format(t))
-            print('t:{:5.4f}, V_dt:[{:5.2f} {:5.2f} {:5.2f}], V:[{:5.4f} {:5.4f} {:5.4f}], U:[{:5.4f} {:5.4f} {:5.2f} {:5.2f}]'.format(t, V_dt[0][0],
-                                                                                                  V_dt[1][0],
-                                                                                                  V_dt[2][0],
-                                                                                                  V[0],
-                                                                                                  V[1],
-                                                                                                  V[2],
-                                                                                                  U[0],
-                                                                                                  U[1],
-                                                                                                  U[2],
-                                                                                                  U[3],
-                                                                                                  ))
+        # if t > 0.999:
+        #     print('_______')
+        # if t < 0.5:
+        #     # print('t:{:5.4f}'.format(t))
+        #     print('t:{:5.4f}, V_dt:[{:5.2f} {:5.2f} {:5.2f}], V:[{:5.4f} {:5.4f} {:5.4f}], U:[{:5.4f} {:5.4f} {:5.2f} {:5.2f}]'.format(t, V_dt[0][0],
+        #                                                                                           V_dt[1][0],
+        #                                                                                           V_dt[2][0],
+        #                                                                                           V[0],
+        #                                                                                           V[1],
+        #                                                                                           V[2],
+        #                                                                                           U[0],
+        #                                                                                           U[1],
+        #                                                                                           U[2],
+        #                                                                                           U[3],
+        #                                                                                           ))
+
+        c, s = np.cos(float(X[3])), np.sin(float(X[3]))
+        R = np.array(((c, -s), (s, c)))
+        Vabs = np.matmul(V[:2], R.transpose())
+
+        return [1, Vabs[0], Vabs[1], V[2], V_dt[0], V_dt[1], V_dt[2]]
+
+    def solveivp_hybrid_lstm_dx_dt(self, t, X):
+        V = [X[4], X[5], X[6]]
+        U = getInput(X[0])
+
+        V_dt = self.vehicle_model.get_accelerations(X[0], V, U)
 
         c, s = np.cos(float(X[3])), np.sin(float(X[3]))
         R = np.array(((c, -s), (s, c)))
---
+++ src/simulator/model/hybrid_lstm_model.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created 10.07.19 10:25
+
+@author: mvb
+"""
+
+import tensorflow as tf
+import numpy as np
+from learned_model.ml_model.rnn_keras import LongShortTermMemoryNetwork
+import time
+from simulator.model.dynamic_mpc_model import DynamicVehicleMPC
+from collections import deque
+
+
+class HybridLSTMModel:
+
+    def __init__(self, model_name='FirstTry'):
+        self.name = "hybrid_lstm"
+
+        # Load the NN
+        self.lstm = LongShortTermMemoryNetwork(model_name=model_name, predict_only=True)
+        self.lstm.load_model()
+        self.lstm.load_checkpoint('best')
+        # self.weights, self.biases = self.lstm.get_weights()
+
+        # Load parameters for normalizing inputs
+        norm_params = self.lstm.load_normalizing_parameters()
+        self.means = norm_params['mean'].values
+        self.stds = norm_params['standard deviation'].values
+        self.sequence_length = 5
+        self.time_step = 0.1
+        self.time_for_next_sequence_element = 0
+        self.disturbance = [0, 0, 0]
+        # self.sequence_length, self.time_step = self.lstm.load_model_parameters() #TODO activate this line as soon as new model is trained!
+
+        self.data_sequence = deque(maxlen=self.sequence_length)
+
+        # Load nominal vehicle model
+        self.mpc = DynamicVehicleMPC()
+
+    def get_name(self):
+        return self.name
+
+    def normalize_input(self, input):
+        return (input - self.means) / self.stds
+
+    def get_accelerations(self, time, initial_velocities=[0, 0, 0], system_inputs=[0, 0, 0, 0]):
+        if time == 0.0:
+            # print('1')
+            self.time_for_next_sequence_element = self.time_step
+            input = np.hstack((initial_velocities, system_inputs))
+            self.data_sequence.append(input)
+        elif time < self.time_step:
+            # print('2')
+            if len(self.data_sequence) < self.sequence_length and self.disturbance == [0, 0, 0]:
+                input = np.hstack((initial_velocities, system_inputs))
+                self.data_sequence.append(input)
+            else:
+                self.disturbance = self.lstm.predict(np.array([self.data_sequence]))
+        elif time >= self.time_for_next_sequence_element:
+            # print('3')
+            self.time_for_next_sequence_element += self.time_step
+            input = np.hstack((initial_velocities, system_inputs))
+            self.data_sequence.append(input)
+            if len(self.data_sequence) == self.sequence_length:
+                self.disturbance = self.lstm.predict(np.array([self.data_sequence]))
+
+        accelerations = self.mpc.get_accelerations(initial_velocities, system_inputs)
+        # print(f'disturbance {self.disturbance}  velocity {initial_velocities}')
+        # print('accelerations', np.array(accelerations).transpose())
+        # print(time)
+        result = np.array(accelerations).transpose() + self.disturbance
+
+        return result[0]
