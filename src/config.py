c6507d4d6bab4089ed0e8403b3937cb281918836
MvBue <vbueremi@student.ethz.ch>
2019-07-11 18:37:46 +0200

    added config file for essential definitions

---
+++ src/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created 11.07.19 13:47
+
+@author: mvb
+"""
+
+directories = dict(
+    root = '/home/mvb/0_ETH/01_MasterThesis/kartsim_files',
+    rawgokartdata = '/home/mvb/0_ETH/01_MasterThesis/Logs_GoKart/LogData/dynamics_newFormat/cuts'
+)
+
+filenames = dict(
+    rawdatatags = 'preproParams.csv'
+)
