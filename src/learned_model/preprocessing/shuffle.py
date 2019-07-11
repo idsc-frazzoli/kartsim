688c2bab48f7d96b5c6577d2ebac4a275264ca0a
MvBue <vbueremi@student.ethz.ch>
2019-07-11 13:26:38 +0200

    added shuffling of lists

--- src/learned_model/preprocessing/shuffle.py
+++ src/learned_model/preprocessing/shuffle.py
@@ -6,6 +6,7 @@ Created 14.06.19 10:05
 @author: mvb
 """
 import numpy as np
+import random
 
 
 def shuffle_dataframe(features, labels, random_seed=None):
@@ -16,6 +17,7 @@ def shuffle_dataframe(features, labels, random_seed=None):
     shuffled_labels = labels.reindex(idx)
     return shuffled_features, shuffled_labels
 
+
 def shuffle_array(features, labels, random_seed=None):
     nr_of_data_points, nr_of_labels = labels.shape
     merged_data = np.hstack((features, labels))
@@ -25,3 +27,19 @@ def shuffle_array(features, labels, random_seed=None):
     shuffled_features = merged_data[:, :-nr_of_labels]
     shuffled_labels = merged_data[:, -nr_of_labels:]
     return shuffled_features, shuffled_labels
+
+
+def shuffle_list(features, labels, random_seed=None):
+    merged_data = []
+    for feature, label in zip(features, labels):
+        merged_data.append([feature, label])
+
+    if random_seed is not None:
+        random.seed(random_seed)
+    random.shuffle(merged_data)
+    shuffled_features = []
+    shuffled_labels = []
+    for shuffled_feature, shuffled_label in merged_data:
+        shuffled_features.append(shuffled_feature)
+        shuffled_labels.append(shuffled_label)
+    return np.array(shuffled_features), np.array(shuffled_labels)
