64fa4533295c39a1c86215305135092aead06640
MvBue <vbueremi@student.ethz.ch>
2019-09-09 22:25:15 +0200

    Add: new MLP with pruning, seperated data preprocessing, removed get_loss_pictures

--- src/learned_model_for_mpc/learning_disturbance_mlp.py
+++ src/learned_model_for_mpc/learning_disturbance_mlp.py
@@ -7,10 +7,13 @@ Created 13.06.19 11:09
 """
 import config
 from data_visualization.data_io import getPKL
+from learned_model_for_mpc.function_library import get_new_features
 from learned_model_for_mpc.ml_models.mlp_keras_mpc import MultiLayerPerceptronMPC
-from learned_model_for_mpc.ml_models.mlp_keras_mpc_additfeatures import MultiLayerPerceptronMPCAdditFeatures
+# from learned_model_for_mpc.ml_models.mlp_keras_mpc_additfeatures import MultiLayerPerceptronMPCAdditFeatures
+from learned_model_for_mpc.ml_models.mlp_keras_sparse_mpc import MultiLayerPerceptronMPCSparse
 from multiprocessing import Pool
 import pandas as pd
+import numpy as np
 import os
 from data_visualization.data_io import getDirectories
 import matplotlib.pyplot as plt
@@ -43,123 +46,186 @@ def main():
     # print(network_settings)
 
     network_settings = [
-        # [2, 32, 'elu', 0.0001],
-        # [2, 32, 'relu', 0.0001],
-        # [2, 32, 'tanh', 0.0001],
-        # [2, 32, 'sigmoid', 0.0001],
-        # [0, 6, None, 0.0],
-        # [0, 6, None, 0.0001],
-        # [0, 6, None, 0.001],
-        # [0, 6, None, 0.01],
-        [0, 6, None, 1],
-        # [4, 64, 'elu', 0.0001],
-        # [4, 64, 'relu', 0.0001],
-        # [4, 64, 'tanh', 0.0001],
-        # [4, 64, 'sigmoid', 0.0001],
-        # [4, 64, 'softplus', 0.0001],
+        # # [2, 32, 'elu', 0.0001],
+        # # [2, 32, 'relu', 0.0001],
+        # # [2, 32, 'tanh', 0.0001],
+        # # [2, 32, 'sigmoid', 0.0001],
+        # # [2, 16, 'softplus', 0.1],
+        # [1, 16, 'tanh', 0.1],
+        # [1, 16, 'tanh', 0.01],
+        # [1, 16, 'tanh', 0.001],
+        # [1, 16, 'tanh', 0.0001],
+        # [1, 16, 'tanh', 0.0],
+        # [1, 16, 'softplus', 0.1],
+        # [1, 16, 'softplus', 0.01],
+        # [1, 16, 'softplus', 0.001],
+        # [1, 16, 'softplus', 0.0001],
+        # [1, 16, 'softplus', 0.0],
+        # [0, 1296, None, 0.0],
+        # [0, 966, None, 0.1],
+        # [0, 966, None, 0.01],
+        # [0, 966, None, 0.001],
+        [0, 12, None, 0.0001],
     ]
+    train_features = train_labels = test_features = test_labels = None
+    train_features, train_labels, test_features, test_labels = get_data_set()
+
     if len(network_settings) >= 5:
         chunks = [network_settings[i::5] for i in range(5)]
+        if train_features is not None:
+            for i,chunk in enumerate(chunks):
+                chunks[i] = chunk + [train_features, train_labels, test_features, test_labels]
         pool = Pool(processes=5)
         pool.map(train_NN, chunks)
     elif len(network_settings) > 1:
         no_settings = len(network_settings)
         chunks = [network_settings[i::no_settings] for i in range(no_settings)]
+        if train_features is not None:
+            for i,chunk in enumerate(chunks):
+                chunks[i] = chunk + [train_features, train_labels, test_features, test_labels]
         pool = Pool(processes=no_settings)
         pool.map(train_NN, chunks)
     else:
+        if train_features is not None:
+            network_settings = network_settings + [train_features, train_labels, test_features, test_labels]
         train_NN(network_settings)
 
-    # get_loss_pictures()
 
 
 def train_NN(network_settings):
     random_state = 45
 
-    path_data_set = os.path.join(config.directories['root'], 'Data/MLPDatasets/20190829-091021_final_data_set_dynamic_directinput')
+    if len(network_settings) == 5:
+        test_labels = network_settings.pop()
+        test_features = network_settings.pop()
+        train_labels = network_settings.pop()
+        train_features = network_settings.pop()
+    else:
+        train_features, train_labels, test_features, test_labels = get_data_set()
+    print(train_features.shape)
+    print(train_labels.shape)
+    print(test_features.shape)
+    print(test_labels.shape)
+    print(len(train_features.columns.values))
+    # for i, name in enumerate(train_features.columns.values):
+    #     print(name)
+
+    i = 1
+    for l, npl, af, reg in network_settings:
+        # name = '{}x{}_{}_reg{}_kin_directinput'.format(l, npl, str(af), str(reg).replace('.', 'p'))
+        # print('----> {}/{} Start training of model with model_type {}'.format(i, len(network_settings), name))
+        # mlp = MultiLayerPerceptronMPC(epochs=1000, learning_rate=1e-3, batch_size=100, random_seed=random_state,
+        #                               model_name=name)
+        # mlp.build_new_model(layers=l, nodes_per_layer=npl, activation_function=af, regularization=reg)
+        # mlp.train_model(train_features, train_labels)
+        # mlp.save_training_history()
+        # mlp.save_model_performance(test_features, test_labels)
+
+        name = '{}x{}_{}_reg{}_50ksample_expotrigopoly2_l1supersparse_athetaonly_directinput'.format(l, npl, af, str(reg).replace('.', 'p'))
+        print('----> {}/{} Start training of model with model_type {}'.format(i, len(network_settings), name))
+        # mlp = MultiLayerPerceptronMPCAdditFeatures(epochs=100000, learning_rate=1e-4, batch_size=100,
+        #                                            random_seed=random_state,
+        #                                            model_name=name)
+        mlp = MultiLayerPerceptronMPCSparse(epochs=500, learning_rate=1e-4, batch_size=100,
+                                                   random_seed=random_state,
+                                                   model_name=name)
+        mlp.build_train_test_model(train_features, train_labels, test_features=test_features, test_labels=test_labels,
+                                   layers=l, nodes_per_layer=npl, activation_function=af, regularization=reg)
+        #
+        print(f'{name} done with training! :)')
+        i += 1
+
+
+def get_data_set():
+    # path_data_set = os.path.join(config.directories['root'], 'Data/MLPDatasets/20190829-091021_final_data_set_dynamic_directinput')
     # path_data_set = os.path.join(config.directories['root'],
-    #                              'Data/MLPDatasets/20190824-183314_trustworthy_bigdata_kinematic_mpc')
-    # path_data_set = os.path.join(config.directories['root'],
-    #                              'Data/MLPDatasets/20190827-223456_trustworthy_bigdata_nomodel')
+    #                              'Data/MLPDatasets/20190829-091514_final_data_set_kinematic_directinput')
+    path_data_set = os.path.join(config.directories['root'],
+                                 'Data/MLPDatasets/20190829-092236_final_data_set_nomodel_directinput')
     train_features = getPKL(os.path.join(path_data_set, 'train_features.pkl'))
     train_labels = getPKL(os.path.join(path_data_set, 'train_labels.pkl'))
     test_features = getPKL(os.path.join(path_data_set, 'test_features.pkl'))
     test_labels = getPKL(os.path.join(path_data_set, 'test_labels.pkl'))
 
+    merged_train = train_features.join(train_labels.iloc[:, 1:])
+    merged_train = merged_train.sample(n=50000, random_state=16)
+    train_features = merged_train.iloc[:, :-3]
+    train_labels = merged_train.iloc[:, -3:]
+
+    merged_test = test_features.join(test_labels.iloc[:,1:])
+    merged_test = merged_test.sample(n=30000, random_state=42)
+    test_features = merged_test.iloc[:, :-3]
+    test_labels = merged_test.iloc[:, -3:]
+
     train_features = train_features[['vehicle vx [m*s^-1]', 'vehicle vy [m*s^-1]', 'pose vtheta [rad*s^-1]',
                                      'turning angle [n.a]',
                                      'acceleration rear axle [m*s^-2]',
                                      'acceleration torque vectoring [rad*s^-2]']]
-    train_labels = train_labels[['disturbance vehicle ax local [m*s^-2]',
-                                 'disturbance vehicle ay local [m*s^-2]',
-                                 'disturbance pose atheta [rad*s^-2]']]
+    train_features = get_new_features(train_features)
+    # train_labels = train_labels[['disturbance vehicle ax local [m*s^-2]',
+    #                              'disturbance vehicle ay local [m*s^-2]',
+    #                              'disturbance pose atheta [rad*s^-2]']]
     test_features = test_features[['vehicle vx [m*s^-1]', 'vehicle vy [m*s^-1]', 'pose vtheta [rad*s^-1]',
                                    'turning angle [n.a]',
                                    'acceleration rear axle [m*s^-2]',
                                    'acceleration torque vectoring [rad*s^-2]']]
-    test_labels = test_labels[['disturbance vehicle ax local [m*s^-2]',
-                               'disturbance vehicle ay local [m*s^-2]',
-                               'disturbance pose atheta [rad*s^-2]']]
+    test_features = get_new_features(test_features)
+    # test_labels = test_labels[['disturbance vehicle ax local [m*s^-2]',
+    #                            'disturbance vehicle ay local [m*s^-2]',
+    #                            'disturbance pose atheta [rad*s^-2]']]
 
-    # train_labels = train_labels[['vehicle ax local [m*s^-2]', 'vehicle ay local [m*s^-2]', 'pose atheta [rad*s^-2]']]
-    # test_labels = test_labels[['vehicle ax local [m*s^-2]', 'vehicle ay local [m*s^-2]', 'pose atheta [rad*s^-2]']]
+    # train_labels = train_labels[
+    #     ['vehicle ax local [m*s^-2]', 'vehicle ay local [m*s^-2]', 'pose atheta [rad*s^-2]']].reset_index(drop=True)
+    # test_labels = test_labels[
+    #     ['vehicle ax local [m*s^-2]', 'vehicle ay local [m*s^-2]', 'pose atheta [rad*s^-2]']].reset_index(drop=True)
 
-    i = 1
-    for l, npl, af, reg in network_settings:
-        # name = '{}x{}_{}_reg{}_kin_directinput_sym'.format(l, npl, af, str(reg).replace('.', 'p'))
-        name = '{}x{}_{}_reg{}_directinput'.format(l, npl, str(af), str(reg).replace('.', 'p'))
-        print('----> {}/{} Start training of model with model_type {}'.format(i, len(network_settings), name))
-        mlp = MultiLayerPerceptronMPC(epochs=1000, learning_rate=1e-3, batch_size=100, random_seed=random_state,
-                                      model_name=name)
-        mlp.build_new_model(layers=l, nodes_per_layer=npl, activation_function=af, regularization=reg)
-        mlp.train_model(train_features, train_labels)
-        mlp.save_training_history()
-        mlp.save_model_performance(test_features, test_labels)
 
-        # mlp = MultiLayerPerceptronMPCAdditFeatures(epochs=1000, learning_rate=1e-3, batch_size=100,
-        #                                            random_seed=random_state,
-        #                                            model_name=name)
-        # mlp.build_train_test_model(train_features, train_labels, test_features=test_features, test_labels=test_labels,
-        #                            layers=l, nodes_per_layer=npl, activation_function=af, regularization=reg)
+    # train_labels = train_labels[
+    #     ['vehicle ax local [m*s^-2]']].reset_index(drop=True)
+    # test_labels = test_labels[
+    #     ['vehicle ax local [m*s^-2]']].reset_index(drop=True)
+    # # feature selection for poly2_l1supersparse
+    # train_features = train_features.iloc[:, [4, 7, 16, 20, 28, 37, 39, 45, 46, 137, 161, 163, 190, 220, 239, 242, 6, 15, 62, 108, 109, 120, 154, 156, 200, 231, 234, 243, 249, 250, ]]
+    # test_features = test_features.iloc[:, [4, 7, 16, 20, 28, 37, 39, 45, 46, 137, 161, 163, 190, 220, 239, 242, 6, 15, 62, 108, 109, 120, 154, 156, 200, 231, 234, 243, 249, 250, ]]
+    # #feature selection for poly3_l1supersparse
+    # train_features = train_features.iloc[:, [12, 20, 162, 549, 551, 553, 1268, 1961, 2023, 1669, 1824, 1856,  ]]
+    # test_features = test_features.iloc[:, [12, 20, 162, 549, 551, 553, 1268, 1961, 2023, 1669, 1824, 1856,  ]]
 
-        print(f'{name} done with training! :)')
-        i += 1
+    # train_labels = train_labels[
+    #     ['vehicle ay local [m*s^-2]']].reset_index(drop=True)
+    # test_labels = test_labels[
+    #     ['vehicle ay local [m*s^-2]']].reset_index(drop=True)
+    # # feature selection for poly2_l1supersparse
+    # train_features = train_features.iloc[:,
+    #                  [23, 51, 53, 61, 70, 77, 78, 99, 143, 181, 196, 203, 204, 226, 247, 1, 2, 24, 27, 49, 50, 57, 58, 73, 81, 100, 110, 151, 173, 209,]]
+    # test_features = test_features.iloc[:,
+    #                 [23, 51, 53, 61, 70, 77, 78, 99, 143, 181, 196, 203, 204, 226, 247, 1, 2, 24, 27, 49, 50, 57, 58, 73, 81, 100, 110, 151, 173, 209, ]]
+    #feature selection for poly3_l1supersparse
+    # train_features = train_features.iloc[:,[485, 537, 648, 909, 1061, 705, 707, 721, 741, 871, 1525, 1685,  ]]
+    # test_features = test_features.iloc[:,[485, 537, 648, 909, 1061, 705, 707, 721, 741, 871, 1525, 1685,  ]]
 
-def get_loss_pictures():
-    # root_folder = '/home/mvb/0_ETH/01_MasterThesis/kartsim/src/learned_model/trained_models'
-    root_folder = os.path.join(config.directories['root'], 'Models/trained_mlp_models_for_mpc')
+    train_labels = train_labels[
+        ['pose atheta [rad*s^-2]']].reset_index(drop=True)
+    test_labels = test_labels[
+        ['pose atheta [rad*s^-2]']].reset_index(drop=True)
+    # feature selection for poly2_l1supersparse
+    train_features = train_features.iloc[:,
+                     [11, 25, 33, 43, 49, 61, 86, 97, 100, 135, 179, 195, 196, 212, 215, 245, 262, 2, 18, 58, 75, 77, 78, 80, 128, 153, 171, 240, 247, 260,]]
+    test_features = test_features.iloc[:,
+                    [11, 25, 33, 43, 49, 61, 86, 97, 100, 135, 179, 195, 196, 212, 215, 245, 262, 2, 18, 58, 75, 77, 78, 80, 128, 153, 171, 240, 247, 260,]]
+    #feature selection for poly3_l1supersparse
+    # train_features = train_features.iloc[:,[343, 457, 485, 741, 1059, 1367, 77, 79, 327, 889, 909, 918,  ]]
+    # test_features = test_features.iloc[:,[343, 457, 485, 741, 1059, 1367, 77, 79, 327, 889, 909, 918,  ]]
 
-    folder_names = getDirectories(root_folder)
-    # print(folder_names)
-    folder_names.remove('logs')
-    # print(folder_names)
-    # folder_names = ['/home/mvb/0_ETH/01_MasterThesis/kartsim/src/learned_model/trained_models/20190624-134333_3x32_relu_reg0p005',
-    #                 '/home/mvb/0_ETH/01_MasterThesis/kartsim/src/learned_model/trained_models/20190624-134333_3x32_relu_reg0p01']
-    for name in folder_names:
-        train_history_dir = os.path.join(root_folder, name, 'training_history')
+    # for name in train_features.columns.values:
+    #     print(name)
+    # train_features = train_features.sort_values(by=['(pose vtheta [rad*s^-1])^(1/2)'])
+    # print(train_features['(pose vtheta [rad*s^-1])^(1/2)'].head())
+    # print(train_features['pose vtheta [rad*s^-1]'].head())
+    # print(train_features['(pose vtheta [rad*s^-1])^(1/2)'].tail())
+    # print(train_features['pose vtheta [rad*s^-1]'].tail())
 
-        if not any([file.endswith('.pdf') for file in os.listdir(train_history_dir)]):
-            hist = pd.DataFrame.from_csv(os.path.join(train_history_dir, 'history.csv'))
-            plt.figure('Mean Squared Error (Loss)')
-            plt.plot(hist[['epoch']].values, hist[['loss', 'val_loss']].values)
-            plt.legend(['Training Loss', 'Validation Loss'])
-            plt.savefig(os.path.join(train_history_dir, 'loss_mean_squ_err.pdf'))
-
-            plt.figure('Mean Absolute Error')
-            plt.plot(hist[['epoch']].values, hist[['mean_absolute_error', 'val_mean_absolute_error']].values)
-            plt.legend(['Training Error', 'Validation Error'])
-            plt.savefig(os.path.join(train_history_dir, 'mean_abs_err.pdf'))
-
-            plt.figure('Coefficient of Determination R^2')
-            plt.plot(hist[['epoch']].values, hist[['coeff_of_determination', 'val_coeff_of_determination']].values)
-            plt.legend(['Training', 'Validation'])
-            plt.savefig(os.path.join(train_history_dir, 'R_squared.pdf'))
-
-            plt.close('all')
-
-    # _path = os.path.join(model_dir, 'training_history')
-    # hist_save_path = os.path.join(save_path, 'history.csv')
-    # hist.to_csv(hist_save_path)
+    return train_features, train_labels, test_features, test_labels
 
 
 if __name__ == '__main__':
