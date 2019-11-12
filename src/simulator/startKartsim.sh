081d4f357213a01f912b1bb9818863198d614d6d
MvBue <vbueremi@student.ethz.ch>
2019-11-12 16:44:37 +0100

    cleanup

--- src/simulator/startKartsim.sh
+++ src/simulator/startKartsim.sh
@@ -8,41 +8,46 @@
 PREPROROOT='/home/mvb/0_ETH/01_MasterThesis/Logs_GoKart/LogData/DataSets'
 PREPROFOLDER=$(cd /home/mvb/0_ETH/01_MasterThesis/Logs_GoKart/LogData/DataSets; ls -t | head -1)
 PREPROFOLDERPATH=$PREPROROOT/$PREPROFOLDER
-#echo $PREPROFOLDERPATH
 
 SAVEPATH="/home/mvb/0_ETH/01_MasterThesis/SimData"
 FOLDERNAME=$(date +"%Y%m%d-%H%M%S")
-FOLDERPATH=$SAVEPATH/$FOLDERNAME
 SIMLOGFILENAMES=()
 PORT="6000"
+
 #select files
 ITER=0
 FIRSTFILE=0
 LASTFILE=10000
 for i in $PREPROFOLDERPATH/*.pkl; do if ((ITER >= $FIRSTFILE && ITER <= $LASTFILE)); then SIMLOGFILENAMES+=($(basename "$i")); fi; ITER=$(( $ITER + 1)); done;
-#SIMLOGFILENAMES=$(cd $PREPROFOLDERPATH; ls -l | egrep -v '^d')
 
 VISUALIZATION=1
 LOGGING=0
 
-# Specify vehicle model to simulate with
-# possible options: "mpc_dynamic", "hybrid_mlp", "hybrid_lstm"
-#VEHICLE_MODEL_TYPE="mpc_dynamic"
-#VEHICLE_MODEL_TYPE="hybrid_mlp"
-VEHICLE_MODEL_TYPE="hybrid_lstm"
+# Specify vehicle model type and the model name to simulate with
+# possible options: "mpc_dynamic", "hybrid_mlp",
+VEHICLE_MODEL_TYPE="mpc_dynamic"
+VEHICLE_MODEL_NAME="original"
 
-# Specify ML model name to be used for model
-# only necessary for vehicle model types "hybrid_mlp" and "hybrid_lstm"
+#VEHICLE_MODEL_TYPE="mpc_kinematic"
 #VEHICLE_MODEL_NAME="original"
 
-# VEHICLE_MODEL_NAME="5x64_relu_reg0p0" #for "hybrid_mlp" model
-# VEHICLE_MODEL_NAME="5x64_relu_reg0p01" #for "hybrid_mlp" model
-# VEHICLE_MODEL_NAME="5x64_relu_reg0p1" #for "hybrid_mlp" model
-#VEHICLE_MODEL_NAME="2x32_linear_reg0p0005" #for "hybrid_mlp" model
-#VEHICLE_MODEL_NAME="2x32_linear_reg0p008" #for "hybrid_mlp" model
-#VEHICLE_MODEL_NAME="2x32_relu_reg0p05" #for "hybrid_mlp" model
+#VEHICLE_MODEL_TYPE="hybrid_mlp"
+# VEHICLE_MODEL_NAME="0x6_None_reg0p0_dyn_symmetric" #for "hybrid_mlp" model
+#VEHICLE_MODEL_NAME="1x16_tanh_reg0p0_dyn_symmetric" #for "hybrid_mlp" model
+# VEHICLE_MODEL_NAME="2x16_softplus_reg0p01_dyn_symmetric" #for "hybrid_mlp" model
 
-VEHICLE_MODEL_NAME="2x32_relu_reg0p01" #for "hybrid_lstm" model
+#VEHICLE_MODEL_TYPE="hybrid_kinematic_mlp"
+# VEHICLE_MODEL_NAME="1x16_tanh_reg0p0_kin_symmetric" #for "hybrid_kinematic_mlp" model
+# VEHICLE_MODEL_NAME="2x16_tanh_reg0p0_kin_symmetric" #for "hybrid_kinematic_mlp" model
+# VEHICLE_MODEL_NAME="3x32_tanh_reg0p0_kin_symmetric" #for "hybrid_kinematic_mlp" model
+# VEHICLE_MODEL_NAME="4x64_tanh_reg0p0_kin_symmetric" #for "hybrid_kinematic_mlp" model
+
+#VEHICLE_MODEL_TYPE="no_model"
+# VEHICLE_MODEL_NAME="1x16_tanh_reg0p0_kin_symmetric" #for "no_model" model
+# VEHICLE_MODEL_NAME="2x16_tanh_reg0p0_kin_symmetric" #for "no_model" model
+# VEHICLE_MODEL_NAME="3x32_tanh_reg0p0_kin_symmetric" #for "no_model" model
+# VEHICLE_MODEL_NAME="4x64_tanh_reg0p0_kin_symmetric" #for "no_model" model
+
 
 SIMTAG=_$VEHICLE_MODEL_TYPE\_$VEHICLE_MODEL_NAME
 SAVEFOLDERPATH=$SAVEPATH/$FOLDERNAME$SIMTAG
@@ -59,7 +64,7 @@ fi
 if [ $LOGGING = 1 ]
 then
     mkdir $SAVEFOLDERPATH
-    for i in "${SIMLOGFILENAMES[@]}"; do cp $PREPROFOLDERPATH/"${i}" $SAVEFOLDERPATH; done
+#    for i in "${SIMLOGFILENAMES[@]}"; do cp $PREPROFOLDERPATH/"${i}" $SAVEFOLDERPATH; done
 
     python3 kartsim_loggerclient.py $PORT $SAVEFOLDERPATH $VEHICLE_MODEL_TYPE $VEHICLE_MODEL_NAME "${SIMLOGFILENAMES[@]}" &
     LOGPID=$!
@@ -67,9 +72,9 @@ fi
 
 #python3 joystick/joystick_client.py &
 #python3 user/simtompc_comparison_client.py &
-python3 user/real_time_sim_client.py $PORT $LOGGING $SAVEFOLDERPATH $PREPROFOLDERPATH "${SIMLOGFILENAMES[@]}" &
+#python3 user/real_time_sim_client.py $PORT $LOGGING $SAVEFOLDERPATH $PREPROFOLDERPATH "${SIMLOGFILENAMES[@]}" &
 #python3 user/dummyClient.py &
-SIMPID=$!
+#SIMPID=$!
 
 exit_script() {
     echo " "
